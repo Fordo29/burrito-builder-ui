a90be7047181d2267232230211021716a6b67887
Christine Rowland <fordo29@gmail.com>
2022-03-08 11:17:09 -0700

    Add Post request and set logic to only add an order if there is at least a name and one ingredient

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,5 +7,20 @@ export const getOrders = () => {
         throw new Error('Something went wrong. Please try again later.')
       }
     })
+}
 
+export const addToOrders = (newOrder) => {
+    return fetch('http://localhost:3001/api/v1/orders', {
+      method: 'POST',
+      body: JSON.stringify(newOrder),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Sorry, an error has occurred! Please try again later.')
+      }
+    })
 }
